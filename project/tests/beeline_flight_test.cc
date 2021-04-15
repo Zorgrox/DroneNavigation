aafa568074268eb0c4e49bfa3ce27da956b309bb
Abdirashid Ahmed <Ahmed844@umn.edu>
2021-04-15 18:46:14 -0500

    added tests for beeline and parbolic

---
+++ project/tests/beeline_flight_test.cc
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include "beeline_flight.h"
+
+#include <iostream>
+
+namespace csci3081
+{
+  class BeelineFlightTest : public ::testing::Test
+  {
+  protected:
+
+  };
+
+  TEST_F(BeelineFlightTest, BeelineFlightTestUpdate)
+  {
+    BeelineFlight* beeline = new BeelineFlight();
+    std::vector<float> pos = { 1.0, 2.0, 3.0 };
+    std::vector<float> target = {4.0, 5.0, 6.0};
+    beeline->SetFlightDetails(pos, target);
+    std::vector<float> direction = {7.0, 8.0, 9.0};
+    std::vector<float> expected_direction = {0.57735, 0.57735, 0.57735};
+    std::vector<float> expected_pos = {9.66, 10.6603, 11.6603};
+    float speedANDdt = 15.0;
+    beeline->FlightUpdate(speedANDdt, pos, direction);
+    ASSERT_NEAR(expected_pos[0], pos[0],0.1);
+    ASSERT_NEAR(expected_pos[1], pos[1],0.1);
+    ASSERT_NEAR(expected_pos[2], pos[2],0.1);
+    ASSERT_NEAR(expected_direction[0], direction[0],0.1);
+    ASSERT_NEAR(expected_direction[1], direction[1],0.1);
+    ASSERT_NEAR(expected_direction[2], direction[2],0.1);
+
+  }
+
+}
