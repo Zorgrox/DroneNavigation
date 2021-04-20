81bb1a2c8ab9a3542f2c993dbb86833157c3ad17
David Johnson <joh13503@umn.edu>
2021-04-20 12:27:10 -0500

    made it so drone will stay on beeline path, not above it

--- project/src/beeline_flight.cc
+++ project/src/beeline_flight.cc
@@ -7,7 +7,7 @@ namespace csci3081 {
   BeelineFlight::BeelineFlight() {  }
 
   void BeelineFlight::FlightUpdate(float speedANDdt, std::vector<float>& pos, std::vector<float>& dir) {
- if (pos.at(1)<280) {pos.at(1)+=.1;} // Cause the drone to rise, to fly over buildings
+ if (pos.at(1)<280) {pos.at(1)+=speedANDdt;} // Cause the drone to rise, to fly over buildings
  else {
 
   //increment drone x,y,z position by the stored direction and it's speed and dt time
@@ -58,9 +58,11 @@ namespace csci3081 {
     std::vector<std::vector<float>> tmpRoute;
     float tripDistance = CalculateDistance(pos,target);
     for (int i(0); i <= 30; i++) {
+
       Vector3D Tmp(pos);
       std::vector<float> currentPosition = pos;
       std::vector<float> newDirection = Tmp.SubtractTwoVectors(target, currentPosition);
+
 
       //saves and normalizes direction vector for later
       Vector3D Direction(newDirection);
@@ -71,7 +73,7 @@ namespace csci3081 {
       //newDirection now contains the x,z movement offset and will be added to the drone's position
       currentPosition = Direction.AddTwoVectors(currentPosition, newDirection);
       // sets the vectors at input refrence pos and dir to the intended position and direction
-
+      currentPosition.at(1)=280;
       tmpRoute.push_back(currentPosition);
 
     }
